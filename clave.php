63de3f1e9422a6a615f419bbfd763cf40aab3224
Milo <bona2675@hotmail.com>
2018-01-15 16:15:09 -0600

    php

---
+++ clave.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+	<title>Query string </title>
+	</head>
+	<body>
+
+	    <?php
+
+        // The value of the variable name is found
+        echo "<h1>Hello " . $_GET["name"] . "</h1>";
+        
+        // The value of the variable age is found
+        echo "<h1>You are " . $_GET["age"] . " years old </h1>";
+
+        ?>
+
+	</body>
+</html>
